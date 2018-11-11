92aa5ad3dfa39c41caf9f87c64e857c4548cc328
olegush <43958767+olegush@users.noreply.github.com>
2018-11-11 16:37:59 +0300

    improved the code

--- coursera.py
+++ coursera.py
@@ -26,24 +26,17 @@ def get_args_parser():
     return args
 
 
-def get_courses_urls(xml_url):
-    courses_xml = requests.get(xml_url, UserAgent().chrome, timeout=(1, 30))
-    root = etree.fromstring(courses_xml.text.encode('utf-8'))
+def get_courses_urls(xml_tree):
     courses_urls_list = []
-    for elements in root.getchildren():
+    for elements in xml_tree.getchildren():
         for url in elements.getchildren():
             courses_urls_list.append(url.text)
     return courses_urls_list
 
 
-def get_course_info(course_url):
+def get_course_info(soup):
     start_date = None
     weeks = None
-    response = requests.get(course_url, UserAgent().chrome, timeout=(1, 30))
-    secs = 5 + round(random.random() * 25)
-    time.sleep(secs)
-    response.encoding = 'utf-8'
-    soup = BeautifulSoup(response.text, 'html.parser')
     title = soup.title
     if title:
         title = title.string.split('|')[0].strip()
@@ -55,10 +48,10 @@ def get_course_info(course_url):
     json_info = soup.find('script', attrs={'type': 'application/ld+json'})
     if json_info:
         json_info = json_info.contents[0].strip()
-        json_graph = json.loads(json_info)['@graph']
-        start_date = json_graph[1]['hasCourseInstance']['startDate']
+        course_info_graph = json.loads(json_info)['@graph']
+        start_date = course_info_graph[1]['hasCourseInstance']['startDate']
         start_date_formatted = datetime.strptime(start_date, '%Y-%m-%d')
-        end_date = json_graph[1]['hasCourseInstance']['endDate']
+        end_date = course_info_graph[1]['hasCourseInstance']['endDate']
         end_date_formatted = datetime.strptime(end_date, '%Y-%m-%d')
         weeks = round((end_date_formatted - start_date_formatted).days / 7)
     rating = soup.find('div', class_='CourseRating')
@@ -73,25 +66,41 @@ def get_course_info(course_url):
     }
 
 
-def output_courses_info_to_xlsx(courses_urls_list, worksheet):
-    for url in courses_urls_list:
-        worksheet.append(list(get_course_info(url).values()))
-    return worksheet
-
-
 if __name__ == '__main__':
     user_args = get_args_parser()
     num_urls = user_args.urls
     filepath = user_args.filename
     coursera_url = 'https://www.coursera.org/sitemap~www~courses.xml'
-    courses_list = get_courses_urls(coursera_url)
+    courses_urls_response = requests.get(
+        coursera_url,
+        UserAgent().chrome,
+        timeout=(1, 30)
+    )
+    courses_xml_tree = etree.fromstring(courses_urls_response.content)
+    courses_list = get_courses_urls(courses_xml_tree)
     print('\nGetting {} random urls from coursera.org ... '.format(num_urls))
     rand_courses_urls = random.sample(courses_list, num_urls)
     print('\nGetting courses info from coursera.org...')
     workbook = Workbook()
     worksheet = workbook.active
     worksheet.append(['title', 'language', 'start date', 'weeks', 'rating'])
-    output_courses_info_to_xlsx(rand_courses_urls, worksheet)
+    for course_url in rand_courses_urls:
+        time.sleep(5 + round(random.random() * 25))
+        courses_response = requests.get(
+            course_url,
+            UserAgent().chrome,
+            timeout=(1, 30)
+        )
+        courses_response.encoding = 'utf-8'
+        soup = BeautifulSoup(courses_response.text, 'html.parser')
+        course_info_dict = get_course_info(soup)
+        worksheet.append([
+            course_info_dict['title'],
+            course_info_dict['language'],
+            course_info_dict['start_date'],
+            course_info_dict['weeks'],
+            course_info_dict['rating']
+        ])
     print('\nWriting data to {} ... '.format(filepath))
     workbook.save(filepath)
     print('\nThe file was saved successfully in the current directory.')
